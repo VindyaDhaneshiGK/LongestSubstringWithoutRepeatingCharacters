5f6071f5e273dd748e4231ac9abc0c2108b21b40
Vindya Gardiyawasam <vindyadgardiyawasam@outlook.com>
2026-01-18 15:38:18 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+int  LengthOfLongestSubString(string s)
+ {
+    int n=0;
+    if(string.IsNullOrEmpty(s))
+    {
+        return 0;
+    }
+    HashSet<char> set = new HashSet<char>();
+    int left = 0, maxLength = 0;
+    for(int right=0;right<s.Length;right++)
+    {
+        while (set.Contains(s[right]))
+        {
+            set.Remove(s[left]);
+            left++;
+        }
+        set.Add(s[right]);
+        maxLength = Math.Max(maxLength,right-left+1);
+
+    }
+
+    return maxLength;
+ }
+int result = 0;
+string input = "abcxabcbb";
+result = LengthOfLongestSubString(input);
+Console.WriteLine("String Passed to Check Length of Long String:"+input);
+if (result == 0)
+{
+    Console.WriteLine("Test failed");
+}
+else
+{
+       Console.WriteLine("Length of longest substring is:"+result);
+}
+
+Console.ReadLine();
